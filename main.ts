5022985c0f60a16fc9ea634d21c05677407e00b3
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-08-05 16:25:44 -0500

    Add boolean ON and OFF

--- main.ts
+++ main.ts
@@ -4,10 +4,24 @@
 //% weight=100 color=#0fbc11 icon=""
 namespace constants {
     /**
-     * return a numeric value of 3.14159
+     * returns a numeric value of 3.14159
      */
     //% block="PI"
     export function pi():number {
         return 3.14159;
+    }
+    /**
+     * returns a boolean value of false
+     */
+    //% block="OFF"
+    export function off():boolean {
+        return false;
+    }
+    /**
+     * returns a boolean value of true
+     */
+    //% block="ON"
+    export function on():boolean {
+        return true;
     }
 } 
