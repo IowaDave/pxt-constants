b765650f1de5027e7cab30074311906fe6cd6d07
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-08-05 16:06:12 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace constants {
+    /**
+     * return a numeric value of 3.14159
+     */
+    //% block="PI"
+    export function pi():number {
+        return 3.14159;
+    }
+} 
