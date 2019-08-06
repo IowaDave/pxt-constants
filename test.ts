fbeb73f347c9ce9bf3a4fe359502f476c5c1fb27
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-08-06 11:17:48 -0500

    Added tests

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.showNumber(constants.pinHigh())
+basic.showNumber(constants.pinLow())
+if (constants.on()) {
+    basic.showString("On!")
+}
+if (!constants.pinLow()) {
+    basic.showString("Off!")
+}
+basic.showNumber(constants.pi())
+basic.showString(constants.pi().toString())
