4942614d1dd35aa6a7a7a43fd7af116a7e07b37e
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-09-04 11:10:04 -0500

    Complete Math constants, change icon

--- main.ts
+++ main.ts
@@ -1,27 +1,109 @@
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon="\uf192"
+//% weight=100 color=#0fbc11 icon="\uf013"
+//% groups=['Math', 'Pin-related', 'Boolean', 'others']
+
 namespace constants {
+
     /**
-     * returns a numeric value of 3.14159
+     * Math constants:
+     *E = 2.718281828459045;
+     *LN2 = 0.6931471805599453;
+     *LN10 = 2.302585092994046;
+     *LOG2E = 1.4426950408889634;
+     *LOG10E = 0.4342944819032518;
+     *PI = 3.141592653589793;
+     *SQRT1_2 = 0.7071067811865476;
+     *SQRT2 = 1.4142135623730951;
      */
-    //% block="PI"
-    export function pi():number {
+
+    /**
+     * returns constant 3.141592653589793
+     */
+    //% block="constPI"
+    //% group="Math"
+    export function constPI(): number {
         return Math.PI;
     }
+
+    /**
+     * returns constant 2.718281828459045;
+     */
+    //% block="constE"
+    //% group="Math"
+    export function constE(): number {
+        return Math.E;
+    }
+
+    /**
+     * returns constant 1.4142135623730951
+     */
+    //% block="constSQRT2"
+    //% group="Math"
+    export function constSQRT2(): number {
+        return Math.SQRT2;
+    }
+
+    /**
+     * returns constant 0.7071067811865476
+     */
+    //% block="constSQRT1_2"
+    //% group="Math"
+    export function constSQRT1_2(): number {
+        return Math.SQRT1_2;
+    }
+
+    /**
+     * returns constant 0.4342944819032518
+     */
+    //% block="constLOG10E"
+    //% group="Math"
+    export function constLOG10E(): number {
+        return Math.LOG10E;
+    }
+
+    /**
+     * returns constant 1.4426950408889634
+     */
+    //% block="constLOG2E"
+    //% group="Math"
+    export function constLOG2E(): number {
+        return Math.LOG2E;
+    }
+
+    /**
+     * returns constant 2.302585092994046
+     */
+    //% block="constLN10"
+    //% group="Math"
+    export function constLN10(): number {
+        return Math.LN10;
+    }
+
+    /**
+     * returns constant 0.6931471805599453
+     */
+    //% block="constLN2"
+    //% group="Math"
+    export function constLN2(): number {
+        return Math.LN2;
+    }
+
     /**
      * returns a boolean value of false
      */
     //% block="OFF"
-    export function off():boolean {
+    //% group="Boolean"
+    export function off(): boolean {
         return false;
     }
     /**
      * returns a boolean value of true
      */
     //% block="ON"
-    export function on():boolean {
+    //% group="Boolean"
+    export function on(): boolean {
         return true;
     }
     /**
@@ -29,6 +111,7 @@ namespace constants {
      * useful to set digital pin 'high' or 'on'
      */
     //% block="pinHigh"
+    //% group="Pin-related"
     export function pinHigh(): number {
         return 1;
     }
@@ -37,6 +120,7 @@ namespace constants {
      * useful to set digital pin 'low' or 'off'
      */
     //% block="pinLow"
+    //% group="Pin-related"
     export function pinLow(): number {
         return 0;
     }
--- test.ts
+++ test.ts
@@ -1,11 +1,23 @@
 // tests go here; this will not be compiled when this package is used as a library
-basic.showNumber(constants.pinHigh())
-basic.showNumber(constants.pinLow())
+
+// pin-related constants
+basic.showString(constants.pinHigh().toString())
+basic.showString(constants.pinLow().toString())
+
+// boolean constants
 if (constants.on()) {
     basic.showString("On!")
 }
-if (!constants.pinLow()) {
-    basic.showString("Off!")
+if (!constants.off()) {
+    basic.showString("Not Off!")
 }
-basic.showNumber(constants.pi())
-basic.showString(constants.pi().toString())
+
+// math constants
+basic.showString(constants.constPI().toString())
+basic.showString(constants.constE().toString())
+basic.showString(constants.constLN10().toString())
+basic.showString(constants.constLN2().toString())
+basic.showString(constants.constLOG10E().toString())
+basic.showString(constants.constLOG2E().toString())
+basic.showString(constants.constSQRT1_2().toString())
+basic.showString(constants.constSQRT2().toString())
